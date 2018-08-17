027a90d8d92068ea369e8fa23cb39c4456ee938c
Alexandre Gabriel y Galan <alexgyg@msn.com>
2018-08-17 02:21:46 +0200

    relations.

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\MessagesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 class MessagesController extends AbstractController
 {
     public function testMessagerie()
     {
+
         return $this->render('tests/messagerie.html.twig', [
             'controller_name' => 'MessagesController',
+        ]);
+    }
+
+    public function tesAllMessages(MessagesRepository $messagesRepository): Response
+    {
+        return $this->render('tests/messagerie.html.twig', [
+            'messages' => $messagesRepository->findAll(),
+            'controller_name' => 'MessagesController'
         ]);
     }
 }
